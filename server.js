1a40d5d0fc1441395556df628a532cd25a49bcc2
tratchapong <tratchapong@gmail.com>
2024-07-02 11:24:20 +0700

    authRoute

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const notFound = require('./middlewares/not-found')
 const errorMw = require('./middlewares/error-mw')
+const authRoute = require('./routes/auth-route')
 const app = express()
+
+app.use('/auth',authRoute )
 
 
 app.use(notFound)
 app.use(errorMw)
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8888
 app.listen(port, ()=>console.log('Server running on',port))
