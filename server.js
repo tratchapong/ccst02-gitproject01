9ffddfb137c78baaeb32c525e908b8098caf557c
tratchapong <tratchapong@gmail.com>
2024-07-02 11:10:37 +0700

    error middleware

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const notFound = require('./middlewares/not-found')
+const errorMw = require('./middlewares/error-mw')
 const app = express()
 
 
 app.use(notFound)
-
+app.use(errorMw)
 const port = process.env.PORT || 8000
 app.listen(port, ()=>console.log('Server running on',port))
