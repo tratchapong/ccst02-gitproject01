0bb7842edc0b0be58a9776ce75e90daf1909367b
tratchapong <tratchapong@gmail.com>
2024-07-02 10:47:11 +0700

    notfound middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const notFound = require('./middlewares/not-found')
 const app = express()
 
+
+app.use(notFound)
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=>console.log('Server running on',port))
