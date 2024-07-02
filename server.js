d9d21e8b7690e1df72a2ef1f472bdc9c20aa8b6a
tratchapong <tratchapong@gmail.com>
2024-07-02 11:28:50 +0700

    todoRoute

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express')
 const notFound = require('./middlewares/not-found')
 const errorMw = require('./middlewares/error-mw')
 const app = express()
+const todoRoute = require('./routes/todo-route')
 
+app.use('/todo', todoRoute)
 
 app.use(notFound)
 app.use(errorMw)
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 9999
 app.listen(port, ()=>console.log('Server running on',port))
